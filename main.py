03f0c9f6163fe5498002dcd4591c50668c388191
FGBxRamel <kretschmer.raziel@gmail.com>
2022-07-30 13:40:32 +0200

    Add votings

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import json
-from tkinter.ttk import Style
-from turtle import title
 import interactions as dc
 from random import randint
 from time import strptime, time, strftime, localtime, mktime
@@ -12,6 +10,7 @@ bot = dc.Client(
 scope_ids = [993913459169300540, 918559612813324340]
 privileged_roles = ["918561303184965702", "918560437958742106"]
 run = False
+emote_chars = ["\U0001F1E6", "\U0001F1E7", "\U0001F1E8", "\U0001F1E9", "\U0001F1EA", "\U0001F1EB", "\U0001F1EC", "\U0001F1ED", "\U0001F1EE", "\U0001F1EF"]
 
 open("data.json", "a").close()
 
@@ -32,31 +31,54 @@ def json_dump(data_dict: dict):
 
 
 global data
-data = implement(json.loads(open("data.json", "r+").read()))
+try:
+    data = implement(json.loads(open("data.json", "r+").read()))
+except json.JSONDecodeError:
+    data = {}
 if not "offers" in data:
     data["offers"] = {}
 if not "count" in data:
     data["count"] = {}
 if not "votings" in data:
     data["votings"] = {}
+json_dump(data)
 
 async def automatic_delete() -> None:
     bot._loop.call_later(86400, run_delete)
-    channel = await get(bot, dc.Channel, channel_id=988492568344014908)
+    offer_channel = await get(bot, dc.Channel, channel_id=988492568344014908)
+    voting_channel = await get(bot, dc.Channel, channel_id=977550305157865472)
     current_time = time()
-    delete_ids = []
+    delete_offer_ids, delete_voting_ids = [], []
     global data
     for id, values in data["offers"].items():
         if values["deadline"] <= current_time:
-            message = await channel.get_message(int(values["message_id"]))
+            message = await offer_channel.get_message(int(values["message_id"]))
             try:
                 await message.delete("[Auto] Cleanup")
             except TypeError:
                 continue
-            delete_ids.append(id)
+            delete_offer_ids.append(id)
             data["count"][values["user_id"]] = data["count"][values["user_id"]] - 1
-    for id in delete_ids:
+    
+    for id, values in data["votings"].items():
+        if values["deadline"] <= current_time:
+            message : dc.Message = await voting_channel.get_message(int(values["message_id"]))
+            try:
+                winner, winner_count = "", 0
+                for reaction in message.reactions:
+                    if int(reaction["count"]) > winner_count:
+                        winner, winner_count = reaction["emoji"]["name"], int(reaction["count"])
+                message_embed: dc.Embed = message.embeds[0]
+                message_embed.description = message_embed.description + f"\n\n**Ergebnis:** {winner}"
+                await message.edit(embeds=message_embed)
+            except TypeError:
+                continue
+            delete_voting_ids.append(id)
+    
+    for id in delete_offer_ids:
         del data["offers"][id]
+    for id in delete_voting_ids:
+        del data["votings"][id]
     json_dump(data)
 
 def run_delete():
@@ -217,7 +239,7 @@ async def create_offer_respone(ctx: dc.CommandContext, title: str, price: str, o
         footer=dc.EmbedFooter(text=identifier)
     )
     channel = await ctx.get_channel()
-    await ctx.send("", ephemeral=True)
+    await ctx.send("Das Angebot wurde entgegen genommen.", ephemeral=True)
     sent_message = await channel.send(embeds=app_embed)
     data["offers"][str(identifier)]["message_id"] = str(sent_message.id)
     data["count"][str(ctx.author.id)] = data["count"][str(ctx.author.id)] + 1
@@ -280,8 +302,9 @@ async def edit_offer_id(ctx: dc.CommandContext, id: str, title: str, text: str):
     if type(title) == None or title == " ":
         title = message_embed.title
     if type(text) == None or text == " ":
-        text = message_embed.description
-    text = f"{text}\n**Preis:** {data['offers'][id]['price']}\n*bearbeitet*"
+        text = message_embed.description + "\n*bearbeitet*"
+    else:
+        text = f"{text}\n**Preis:** {data['offers'][id]['price']}\n*bearbeitet*"
     message_embed.title = title
     message_embed.description = text
     await offer_message.edit(embeds=message_embed)
@@ -319,8 +342,6 @@ async def edit_offer_id(ctx: dc.CommandContext, id: str, title: str, text: str):
 )
 async def votings(ctx : dc.CommandContext, aktion : str):
     if aktion == "create":
-        if not str(ctx.author.id) in data["votings"]:
-            data["votings"][str(ctx.author.id)] = {}
         create_voting_modal = dc.Modal(
             title="Abstimmung erstellen",
             custom_id = "mod_create_voting",
@@ -329,7 +350,8 @@ async def votings(ctx : dc.CommandContext, aktion : str):
                     style = dc.TextStyleType.PARAGRAPH,
                     label = "Welch Volksentscheid wollt ihr verkünden?",
                     custom_id = "create_voting_text",
-                    required = True
+                    required = True,
+                    placeholder="Liebe Mitbürger..."
                 ),
                 dc.TextInput(
                     style = dc.TextStyleType.SHORT,
@@ -337,15 +359,152 @@ async def votings(ctx : dc.CommandContext, aktion : str):
                     custom_id = "create_voting_count",
                     required = True,
                     max_length = 2
+                ),
+                dc.TextInput(
+                    style=dc.TextStyleType.SHORT,
+                    label="Wie lange läuft die Abstimmung?",
+                    custom_id="create_voting_deadline",
+                    required=True,
+                    max_length=2
                 )
             ]
         )
         await ctx.popup(create_voting_modal)
     elif aktion == "delete":
-        pass
+        delete_modal = dc.Modal(
+            title="Abstimmung löschen",
+            custom_id="mod_delete_voting",
+            components=[
+                dc.TextInput(
+                    style=dc.TextStyleType.SHORT,
+                    label="ID der Abstimmung",
+                    custom_id="delete_voting_id",
+                    required=True,
+                    min_length=4,
+                    max_length=4
+                )
+            ]
+        )
+        await ctx.popup(delete_modal)
     elif aktion == "edit":
-        pass
+        edit_voting_modal = dc.Modal(
+            title="Abstimmung bearbeiten",
+            custom_id="mod_edit_voting",
+            components=[
+                dc.TextInput(
+                    style=dc.TextStyleType.SHORT,
+                    label="ID der Abstimmmung",
+                    custom_id="edit_voting_id",
+                    required=True,
+                    min_length=4,
+                    max_length=4
+                ),
+                dc.TextInput(
+                    style=dc.TextStyleType.PARAGRAPH,
+                    label="Abstimmungstext",
+                    custom_id="edit_voting_text",
+                    required=True
+                )
+            ]
+        )
+        await ctx.popup(edit_voting_modal)
 
 @bot.modal("mod_create_voting")
-async def create_voting_response(ctx : dc.CommandContext, text : str, count : str):
-    pass
+async def create_voting_response(ctx : dc.CommandContext, text : str, count : str, deadline : str):
+    if int(count) > 10:
+        await ctx.send("Entschuldige, mehr als 10 Möglichkeiten sind aktuell nicht verfügbar.", ephemeral=True),
+        return
+    identifier = randint(1000, 9999)
+    while identifier in data["votings"]:
+        identifier = randint(1000, 9999)
+    
+    data["votings"][str(identifier)] = {}
+    data["votings"][str(identifier)]["user_id"] = str(ctx.author.id)
+
+    count = 2 if int(count) < 2 else count
+    end_time = time() + int(deadline) * 86400
+    data["votings"][str(identifier)]["deadline"] = end_time
+    end_time = strftime("%d.%m.") + "- " + \
+        strftime("%d.%m.", localtime(end_time))
+    
+    server : dc.Guild = await ctx.get_guild()
+    minecrafter_role : dc.Role = await server.get_role(995073183445168179)
+    voting_embed = dc.Embed(
+        title= "Liebe Mitbürger",
+        description=f"\n{text}",
+        color=0xdaa520,
+        author=dc.EmbedAuthor(
+        name=f"{ctx.author.user.username}, {end_time} ({deadline} Tage)"),
+        footer=dc.EmbedFooter(text=identifier)
+    )
+    channel = await ctx.get_channel()
+    await ctx.send("Das Angebot wurde entgegen genommen.", ephemeral=True)
+    sent_message = await channel.send(content = minecrafter_role.mention, embeds=voting_embed)
+    emote_index = 0
+    global emote_chars
+    while int(count) > emote_index:
+        await sent_message.create_reaction(emote_chars[emote_index])
+        emote_index += 1
+    data["votings"][str(identifier)]["message_id"] = str(sent_message.id)
+    json_dump(data)
+
+@bot.modal("mod_delete_voting")
+async def delete_voting_response(ctx : dc.CommandContext, id : str):
+    try:
+        int(id)
+    except ValueError:
+        await ctx.send("Die ID hat ein fehlerhaftes Format!", ephemeral=True)
+        return
+    except BaseException as e:
+        await ctx.send(
+            f"Oops, etwas ist schief gegangen! Fehler: {e}", ephemeral=True)
+        return
+    if not id in data["votings"]:
+        await ctx.send("Diese ID existiert nicht oder die Abstimmung ist vorbei!", ephemeral=True)
+        return
+    user_privilege = False
+    for role in ctx.author.roles:
+        if str(role) in privileged_roles:
+            user_privilege = True
+            break
+    if not data["votings"][id]["user_id"] == str(ctx.author.id) and not user_privilege:
+        await ctx.send("Du bist nicht berechtigt diese Abstimmung zu löschen!",
+                       ephemeral=True)
+        return
+    votings_channel: dc.Channel = await ctx.get_channel()
+    voting_message: dc.Message = await votings_channel.get_message(data["votings"][id]["message_id"])
+    await voting_message.delete(reason=f"[Manuell] {ctx.author.user.username}")
+    del data["votings"][id]
+    json_dump(data)
+    await ctx.send("Die Abstimmung wurde gelöscht.", ephemeral=True)
+
+@bot.modal("mod_edit_voting")
+async def edit_voting_response(ctx : dc.CommandContext, id : str, text : str):
+    try:
+        int(id)
+    except ValueError:
+        await ctx.send("Die ID hat ein fehlerhaftes Format!", ephemeral=True)
+        return
+    except BaseException as e:
+        await ctx.send(
+            f"Oops, etwas ist schief gegangen! Fehler: {e}", ephemeral=True)
+        return
+    if not id in data["votings"]:
+        await ctx.send("Diese ID existiert nicht oder die Abstimmung ist vorbei!", ephemeral=True)
+        return
+    if not data["votings"][id]["user_id"] == str(ctx.author.id):
+        await ctx.send("Du bist nicht berechtigt diese Abstimmung zu bearbeiten!",
+                       ephemeral=True)
+        return
+    voting_channel: dc.Channel = await ctx.get_channel()
+    voting_message: dc.Message = await voting_channel.get_message(data["votings"][id]["message_id"])
+    message_embed: dc.Embed = voting_message.embeds[0]
+    text = message_embed.description if type(text) == None or text == " " else text
+    text = text + "\n*bearbeitet*"
+    message_embed.description = text
+    server : dc.Guild = await ctx.get_guild()
+    minecrafter_role : dc.Role = await server.get_role(995073183445168179)
+    await voting_message.edit(content = minecrafter_role.mention, embeds=message_embed)
+    await ctx.send("Das Angebot wurde bearbeitet.", ephemeral=True)
+
+bot.start()
